a5dff48c35b19217e0c65536f6e2f5a21fe1001e
1gn1z_el_angel <kebor327@gmail.com>
2021-02-14 19:24:42 -0600

    2a. variable banner

--- Citas_APA.py
+++ Citas_APA.py
@@ -1,3 +1,7 @@
 # Importacion de Pyfiglet para el banner (Inicial con mayusculas!)
 
 from Pyfiglet import Figlet
+
+# Indicamos la fuente para el banner, guardada en una variable
+
+banner = Figlet(font="graffiti")
