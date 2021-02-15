eaa59177ab1dd2567d8550cefba2592539272ae6
1gn1z_el_angel <kebor327@gmail.com>
2021-02-14 20:35:01 -0600

    6a. Cita artículo web funcional :D

--- Citas_APA.py
+++ Citas_APA.py
@@ -19,30 +19,56 @@ print('4.- Si tu fuente es un artículo de ENCICLOPEDIA')
 print('5.- Si tu fuente es un artículo o capítulo de un LIBRO')
 print('6.- Si tu fuente es un artículo de PÁGINA WEB')
 print('7.- Si tu fuente es el artículo de un PERIÓDICO, un JOURNAL (Publicación académica especializada.) o REVISTA en una BASE DE DATOS ')
-print('\n8.- Para SALIR')
+print('\n8.- MENU')
+print('\n9.- Para SALIR')
 print()
 print()
 
-opcion = None
 
-while opcion != '8':
-    if opcion == '1':
-        print('opcion 1 elegida')
-    elif opcion == '2':
-        print('opcion 2')
-    elif opcion == '3':
-        print('opcion 3')    
-    elif opcion == '4':
-        print('opcion 4')   
-    elif opcion == '5':
-        print('opcion 5')
-    elif opcion == '6':
-        print('opcion 6')
-    elif opcion == '7':
-        print('opcion 7')  
-    elif opcion == '8':
-        print('Salir')      
-    else:
-        print('opcion invalida, favor de verificar')
+def menu():
+    opcion = None
+    while opcion != '8':
+        if opcion == '1':
+            print('opcion 1 elegida')
+        elif opcion == '2':
+            print('opcion 2')
+        elif opcion == '3':
+            print('opcion 3')    
+        elif opcion == '4':
+            print('opcion 4')   
+        elif opcion == '5':
+            print('opcion 5')
+        elif opcion == '6':
+            print()
+            autor = input('Autor(es). En el campo "Autor" sólo se escribe el apellido paterno y de los nombres, \n'
+            'únicamente se escribe la letra inicial abreviada con punto. (Ejemplo: Okuda, M.).\n')
+            anio = input('Año de publicación:\n')
+            titulo = input('Título del artículo:\n')
+            fecha = input('Fecha de recuperación del documento: \n'
+            'La fecha va en el formato: Mes, día, año(Ejemplo: diciembre 20, 2007)\n')
+            asociacion = input('Asociación que publica el artículo: \n')
+            url = input('URL: \n')
+            print('Tu ficha es:')
+#            print(autor + titulo + fecha + asociacion + url)
+            print(f'{autor}. ({anio}). {titulo}. {fecha}, de {asociacion} Sitio web: {url}')
+            print('\n')
 
-    opcion = input('Elige una opcion: ')
+
+
+        elif opcion == '7':
+            print('opcion 7')
+        elif opcion == '8':
+            opciones()
+        elif opcion == '9':
+            print('Salir')      
+        else:
+            print('opcion invalida, favor de verificar')
+
+        opcion = input('Elige una opcion: ')
+
+
+
+
+
+if __name__ == "__main__":
+    menu()
