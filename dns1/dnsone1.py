4d05dac39d28d1548d95a48a72aae621befe8181
1gn1z_el_angel <kebor327@gmail.com>
2021-03-11 17:38:51 -0600

    1a. Menu, menuloop y exit

---
+++ dns1/dnsone1.py
@@ -0,0 +1,61 @@
+import dns.resolver
+from collections import OrderedDict
+from pyfiglet import Figlet
+
+banner = Figlet(font="standard")
+
+# Imprimimos el texto que queramos como banner
+print(banner.renderText('Dns-One'))
+print(banner.renderText('by  1gn1z'))
+
+
+def menu_loop():
+    """Menu"""
+    accion = None
+    while accion != 's':
+        for key, value in menu.items():
+
+            print((f'{key}) {value.__doc__}'))
+        accion = input('\nOpcion: ').lower().strip()
+
+        if accion in menu:
+            menu[accion]()
+
+
+def exit():
+    """Exit"""
+    accion = input('Are you sure? [Y/n] '.lower().strip())
+    if accion == 'y':
+        sys.exit(1)
+    else:
+        print('\n\n')
+        menu_loop()
+
+
+
+def host_address():
+    """Host address"""
+
+menu = OrderedDict([
+    ('a', host_address),
+    ('b', authoritative_name_server),
+    ('c', mail_destination),
+    ('d', mail_forwarder),
+    ('e', canonical_name_for_alias),
+    ('f', start_of_a_zone_of_authority),
+    ('g', mailbox_domain_name),
+    ('h', mail_group_member),
+    ('i', mail_rename_domain_name),
+    ('j', null_rr),
+    ('k', well_known_service_description),
+    ('l', domain_name_pointer),
+    ('m', host_information),
+    ('n', mailbox_or_mail_list_information),
+    ('o', mail_exchange),
+    ('p', text_strings),
+    ('q', transfer_of_an_entire_zone),
+    ('r', mailbox_related_records),
+    ('s', mail_agent_rr),
+    ('t', all_records)
+    ('x', exit)
+])
