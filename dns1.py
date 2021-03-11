69975a6f573540d7d3754de52b9f6915c15e98da
1gn1z_el_angel <kebor327@gmail.com>
2021-03-11 16:21:36 -0600

    2a. Se puede ingresar cualquier url

--- dns1.py
+++ dns1.py
@@ -3,7 +3,8 @@ import dns.resolver
 def main():
 	try:
 		# El query toma 2 parametros, la página y el código de dns
-		target = dns.resolver.query("www.cicap.edu.mx", "MD")
+		target1 = input('Ingresa la página >>> ')
+		target = dns.resolver.query(target1, "A")
 		for i in target:
 			print(i)
 	except:
